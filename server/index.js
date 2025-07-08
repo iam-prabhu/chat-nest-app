080a858deca8a9b27540c6d02fe5678929428f51
Prabhupratik Pattanaik <151743879+iam-prabhu@users.noreply.github.com>
2025-07-09 00:16:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(
 );
 app.use((req, res, next) => {
    
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Origin', 'https://chatnest-25qa.onrender.com'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
